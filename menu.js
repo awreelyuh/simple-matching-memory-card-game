39275aa7efb6a666f86348bb499422f0ed14fd51
Aurelia Ang <findaurelia@live.com>
2023-03-26 15:56:20 -0400

    edited viewport size for tablet

--- menu.js
+++ menu.js
@@ -1,6 +1,6 @@
 function toggleMenu() {
   const x = document.getElementById("nav-links");
-  if (x.style.maxWidth = "600px" && x.style.display === "block") {
+  if (x.style.maxWidth = "768px" && x.style.display === "block") {
     x.style.display = "none";
   } else {
     x.style.display = "block";
