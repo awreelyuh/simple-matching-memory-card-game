e64e1edccba7505b9511c3c604716ae1b22c3298
Aurelia Ang <findaurelia@live.com>
2023-03-28 00:09:36 -0400

    completed flipcard js

---
+++ cards.js
@@ -0,0 +1,5 @@
+const cards = document.querySelectorAll('.card');
+function flipCard() {
+    this.classList.toggle('is-flipped');
+}
+cards.forEach((card) => card.addEventListener('click', flipCard));
