0e820fe4a92e85c8240dcfe7c55176b969c58c03
Aurelia Ang <findaurelia@live.com>
2023-04-01 02:04:54 -0400

    finished accordion functionality and styling

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 const accordion = document.getElementsByClassName("accordion");
-
 for (i = 0; i < accordion.length; i++) {
   accordion[i].addEventListener("click", function() {
     this.classList.toggle("active");
