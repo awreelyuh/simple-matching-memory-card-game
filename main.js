294df7642e3fa0c7bade542b64e107a0281a0b19
Aurelia Ang <findaurelia@live.com>
2023-04-01 02:02:17 -0400

    animated the accordion using js

--- main.js
+++ main.js
@@ -4,11 +4,11 @@ for (i = 0; i < accordion.length; i++) {
   accordion[i].addEventListener("click", function() {
     this.classList.toggle("active");
     const panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
     } else {
-      panel.style.display = "block";
-    }
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
   });
 }
 
